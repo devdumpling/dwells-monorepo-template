92e59cc3955a76c4e632a4092f718a2c632f4071
Devon Wells <devon.wellsa@gmail.com>
2023-04-08 08:19:02 -0400

    feat: docs use mdxRs and typedRoutes

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,7 +1,15 @@
 /**
  * @type {import('next').NextConfig}
  */
+const nextConfig = {
+  experimental: {
+    mdxRs: true,
+    typedRoutes: true,
+  },
+};
+
 const withNextra = require("nextra")({
+  ...nextConfig,
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
